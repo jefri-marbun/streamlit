ef5eab5cd326667066c46e8d9c6805f32b423354
jefri-marbun <80641117+jefri-marbun@users.noreply.github.com>
2022-09-03 14:46:36 +0700

    Update co.py

--- co.py
+++ co.py
@@ -3,8 +3,5 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
+input = st.text_input('nama')
+st.write(nama)
