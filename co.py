f6925dc274c56f93b0e5eb228c70f6d023b646fe
jefri-marbun <80641117+jefri-marbun@users.noreply.github.com>
2022-09-03 14:44:40 +0700

    Update co.py

--- co.py
+++ co.py
@@ -2,3 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
